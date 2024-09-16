c9a1e0577043b200481a5592678d7a6b23a5aca3
Luan Himmlisch <luan@himmlisch.com.mx>
2024-09-15 22:34:46 -0600

    Added --no-commit option & exec via symfony

--- src/Commands/CleanCode.php
+++ src/Commands/CleanCode.php
@@ -3,11 +3,13 @@
 namespace Weblabor\CodeStandars\Commands;
 
 use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
 
 class CleanCode extends Command
 {
-    public $signature = 'clean:code';
-    public $description = 'Execute the code standars';
+    public $signature = 'clean:code 
+                         {--no-commit : Don\'t execute git commit}';
+    public $description = 'Execute the code standards';
 
     public function handle(): int
     {
@@ -15,10 +17,9 @@ class CleanCode extends Command
         $this->executeCsFixer();
 
         // Execute commands
-        $this->executeCommand('optimize:namespaces');
-        // $this->executeCommand('optimize:remove-comments');
-        $this->executeCommand('optimize:traits-one-line');
-        $this->executeCommand('optimize:variables-one-line');
+        $this->callAndCommit('optimize:namespaces');
+        $this->callAndCommit('optimize:traits-one-line');
+        $this->callAndCommit('optimize:variables-one-line');
 
         $this->executeCsFixer();
         return self::SUCCESS;
@@ -27,15 +28,15 @@ class CleanCode extends Command
     private function executeCsFixer()
     {
         // Execute laravel Pint
-        exec("./vendor/bin/pint --config vendor/weblabormx/weblabor-cs/pint.json");
+        $this->exec("./vendor/bin/pint --config vendor/weblabormx/weblabor-cs/pint.json");
         $this->commit("Laravel Pint");
 
         // Execute CS Fixer
-        exec("php php-cs-fixer.phar fix . --config vendor/weblabormx/weblabor-cs/.php-cs-fixer.php");
+        $this->exec("php php-cs-fixer.phar fix . --config vendor/weblabormx/weblabor-cs/.php-cs-fixer.php");
         $this->commit("PHP CS Fixer");
     }
 
-    private function executeCommand(string $command): void
+    private function callAndCommit(string $command): void
     {
         $this->call($command);
         $this->commit($command);
@@ -43,10 +44,28 @@ class CleanCode extends Command
 
     private function commit($name)
     {
-        if(!config('weblabor-cs.automatic_commit')) {
+        if (
+            !config('weblabor-cs.automatic_commit') ||
+            $this->option('no-commit')
+        ) {
             return;
         }
-        exec('git add .');
-        exec('git commit -m "'.$name.'"');
+
+        if ($this->exec('git add .')) {
+            $this->exec('git commit -m "${:COMMIT_MSG}"', ['COMMIT_MSG' => $name]);
+        }
+    }
+
+
+    private function exec(string $command, array $args = []): bool
+    {
+        $process = Process::fromShellCommandline($command, base_path(), [...$_ENV, ...$args]);
+
+        $process->run();
+
+        if ($process->isSuccessful()) return true;
+
+        $this->error($process->getErrorOutput());
+        return false;
     }
 }
